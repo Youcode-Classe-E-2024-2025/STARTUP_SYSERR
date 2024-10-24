d0336fca5f924283946d07f24fb6211010fce233
zakariaeelhassad <zakariaelhassad031@gmail.com>
2024-10-24 15:05:08 +0100

    Updated design with CSS and improved interactions with JS

--- assets/js/Faq.js
+++ assets/js/Faq.js
@@ -4,10 +4,12 @@ answers.forEach((accordion) => {
     accordion.addEventListener("click", () => {
         if (accordion.classList.contains("active")) {
             accordion.classList.remove("active");
+            accordion.style.animation = "accordslideback 0.2s ease forwards";
         } else {
             // Close all other accordions
             answers.forEach((item) => item.classList.remove("active"));
             accordion.classList.add("active");
+            accordion.style.animation = "accordslide 0.2s ease forwards";
         }
     });
 });
