de14f19acaf6b4b727115d3fa74c83c835c4d852
medlaq777 <65530218+medlaq777@users.noreply.github.com>
2024-10-25 17:13:31 +0100

    Update Faq.js new version

--- assets/js/Faq.js
+++ assets/js/Faq.js
@@ -1,13 +1,21 @@
 let answers = document.querySelectorAll(".accordion");
 answers.forEach((accordion) => {
-  accordion.addEventListener("click", () => {
-    if (accordion.classList.contains("active")) {
-      accordion.classList.remove("active");
-      accordion.style.animation = "accordslideback 0.2s ease forwards";
-    } else {
-      answers.forEach((item) => item.classList.remove("active"));
-      accordion.classList.add("active");
-      accordion.style.animation = "accordslide 0.2s ease forwards";
-    }
-  });
+    accordion.addEventListener("click", () => {
+        if (accordion.classList.contains("active")) {
+            accordion.classList.remove("active");
+            accordion.style.animation = "none"; 
+            setTimeout(() => {
+                accordion.style.animation = "accordslideback 0.2s ease forwards";
+            }, 10);
+        } else {
+            answers.forEach((item) => {
+                item.classList.remove("active");
+                item.style.animation = "none";
+            });
+            accordion.classList.add("active");
+            setTimeout(() => {
+                accordion.style.animation = "accordslide 0.2s ease forwards";
+            }, 10);
+        }
+    });
 });
