0fa30c3008f17fbb264a46da0a18e5fb2c8341b0
Asmae <asmaeelhamzaoui77@gmail.com>
2024-10-25 10:20:26 +0100

    l'ajout des dernières modifications

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,16 +1,16 @@
 const carouselImages = document.querySelector('.carousel-images');
-let imageDivs = Array.from(document.querySelectorAll('.carousel-image'));
+    let imageDivs = Array.from(document.querySelectorAll('.carousel-image'));
 
-function moveSlide(direction) {
-    if (direction === 1) {
-        const firstElement = imageDivs.shift();
-        imageDivs.push(firstElement);
-    } else if (direction === -1) {
-        const lastElement = imageDivs.pop();
-        imageDivs.unshift(lastElement);
+    function moveSlide(direction) {
+        if (direction === 1) {
+            const firstElement = imageDivs.shift();
+            imageDivs.push(firstElement);
+        } else if (direction === -1) {
+            const lastElement = imageDivs.pop();
+            imageDivs.unshift(lastElement);
+        }
+        carouselImages.innerHTML = '';
+        imageDivs.forEach(div => carouselImages.appendChild(div));
     }
-    carouselImages.innerHTML = '';
-    imageDivs.forEach(div => carouselImages.appendChild(div));
-}
 
-setInterval(() => moveSlide(1), 2000);
+    setInterval(() => moveSlide(1), 2000);
